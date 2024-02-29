b51383df14ff90fa33b7ba3ce39f0bf1f8c62789
weiaoxin <weiaoxin@163.com>
2024-02-29 09:52:24 +0800

    update zsh install shell

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,6 +1,5 @@
 sudo apt-get install zsh
-git clone https://gitee.com/mirrors/oh-my-zsh.git ~/.oh-my-zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s /bin/zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
